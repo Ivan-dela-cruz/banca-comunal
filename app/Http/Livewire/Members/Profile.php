8cbb34ac00b85804cabe21ace95a1b5122c30cac
Ivan <canarivan495@gmail.com>
2021-07-12 21:32:31 -0500

    add profile

--- app/Http/Livewire/Members/Profile.php
+++ app/Http/Livewire/Members/Profile.php
@@ -3,16 +3,24 @@
 namespace App\Http\Livewire\Members;
 
 use Livewire\Component;
-
+use App\Models\Credits;
+use App\Models\Member;
+use App\Models\DetailAmortization;
+use Carbon\Carbon;
 class Profile extends Component
 {
-    public $member_id,$member;
+    public $member_id,$member,$listCredits=[],$listPayments=[];
 
     public function mount($id)
     {
-        $this->credit_request = CreditRequest::find($id);
-        $this->member = Member::find($this->credit_request->member_id);
-        $this->member_detail = $this->member->detail;
+        $now =  Carbon::now();
+        $this->member = Member::find($id);
+        $this->listCredits = Credits::where('member_id', $id)->orderBy('created_at', 'desc')->get();
+        $this->listPayments = DetailAmortization::join('amortizations','detail_amortizations.amortization_id','=','amortizations.id')
+        ->select('detail_amortizations.*')
+        ->whereMonth('detail_amortizations.payment_date', $now->month)
+        ->where('amortizations.member_id', $id)
+        ->get();
     }
 
     public function render()
--- resources/views/pages/profile-overview-1.blade.php
+++ resources/views/pages/profile-overview-1.blade.php
@@ -1,7 +1,7 @@
 @extends('../layout/' . $layout)
 
 @section('subhead')
-    <title>Profile - Midone - Tailwind HTML Admin Template</title>
+    <title>Perfil Cliente</title>
 @endsection
 
 @section('subcontent')
